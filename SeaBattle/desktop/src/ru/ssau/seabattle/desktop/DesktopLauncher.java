bdd258e35a10be3462c8da448e4b0b36dc518d28
Beywer <Beywer@idea-pc>
2014-12-12 11:30:31 -0300

    Чистый градля

---
+++ SeaBattle/desktop/src/ru/ssau/seabattle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.ssau.seabattle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.ssau.seabattle.SeaBattle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SeaBattle(), config);
+	}
+}
